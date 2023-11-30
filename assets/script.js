0fb3c8d1a0f1d5927f4b07996a7c32c2aa29529a
DESKTOP-I5NVTDK\Long <vanlong5436@gmail.com>
2023-11-30 21:21:16 +0700

    fixbug, add touch, add navigation control

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,8 @@ class Game {
     this.score = 0;
     this.width = width;
     this.height = height;
+    this.xStart = 0;
+    this.yStart = 0;
     this.context = context;
     this.spacing = 10;
     this.sizeItem = 100;
@@ -40,6 +42,26 @@ class Game {
       4096: 'rgb(254,61,62)',
       unknown: 'rgb(237,42,30)',
     };
+
+    //
+
+    this.xStart = (this.width - this.sizeContainer) / 2;
+    this.yStart = 150;
+
+    if (width <= 480) {
+      this.xStart = this.spacing / 2;
+
+      this.sizeItem =
+        Math.round(this.width / this.sizeGame) -
+        (this.spacing + this.spacing / 2);
+      console.log(this.sizeItem);
+
+      //   // this.sizeItem = width / (4 + 10 * 5);
+      //   this.sizeItem = 80;
+      this.sizeContainer =
+        this.sizeItem * this.sizeGame + this.spacing * (this.sizeGame + 1);
+      //   console.log(canvas.width);
+    }
 
     this.createArr2D();
     this.getAllItemHaveZeroValue();
@@ -78,7 +100,7 @@ class Game {
     this.drawGame();
   }
 
-  renderItem(x, y, value) {
+  drawItem(x, y, value) {
     // draw background
     this.context.beginPath();
     this.context.fillStyle = this.colors[0];
@@ -89,17 +111,23 @@ class Game {
       this.context.fillStyle = this.colors.unknown;
     }
 
-    this.context.roundRect(x + 100, y + 100, this.sizeItem, this.sizeItem, [4]);
+    this.context.roundRect(
+      x + this.xStart,
+      y + this.yStart,
+      this.sizeItem,
+      this.sizeItem,
+      [4]
+    );
     this.context.fill();
 
     // draw text
     let textFit = 0;
-    this.context.font = '48px sans-serif';
+    this.context.font = '3rem sans-serif';
     this.context.fillStyle =
       value > 4 ? this.colors.textLight : this.colors.textDark;
 
-    const text = this.context.measureText(value);
-    const textWidth = Math.round(text.width);
+    let text = this.context.measureText(value);
+    let textWidth = Math.round(text.width);
 
     if (this.colors[value]) {
       this.context.fillStyle = this.colors[value].color;
@@ -107,16 +135,34 @@ class Game {
     // this.context.textAlign = 'center';
     this.context.textBaseline = 'top';
 
-    if (textWidth > this.sizeItem) {
-      textFit = textWidth - this.sizeItem;
-      this.context.font = `${48 - textFit / 2}px sans-serif`;
+    if (text.actualBoundingBoxRight > this.sizeItem) {
+      textFit = text.width - this.sizeItem;
+      this.context.font = `${3 - (textFit * 3) / 100}rem sans-serif`;
+      text = this.context.measureText(value);
+      textWidth = Math.round(text.width);
     }
 
     this.context.fillText(
       value === 0 ? '' : value,
-      x + 100 + (this.sizeItem - textWidth) / 2 + textFit / 2,
-      y + 100 + 30
+      x + this.xStart + ((text.width - this.sizeItem) / 2) * -1,
+      y + this.yStart + (this.sizeItem - Math.abs(text.alphabeticBaseline)) / 2
     );
+
+    // if (text.actualBoundingBoxRight > this.sizeItem) {
+    //   textFit = text.actualBoundingBoxRight - this.sizeItem;
+    //   this.context.font = `${48 - textFit / 2}px sans-serif`;
+    //   text = this.context.measureText(value);
+    //   textWidth = Math.round(text.width);
+    // }
+
+    // this.context.fillText(
+    //   value === 0 ? '' : value,
+    //   x +
+    //     this.xStart +
+    //     (this.sizeItem - text.actualBoundingBoxRight) / 2 +
+    //     textFit / 2,
+    //   y + this.yStart + (this.sizeItem - Math.abs(text.alphabeticBaseline)) / 2
+    // );
   }
 
   clearDraw() {
@@ -191,61 +237,140 @@ class Game {
 
     this.context.beginPath();
     this.context.fillStyle = this.colors[0];
-    this.context.roundRect(x, y, 400, 400, [10]);
+    this.context.roundRect(x, y, this.sizeContainer, 400, [10]);
     this.context.fill();
 
     this.context.font = '50px sans-serif';
     this.context.fillStyle = this.colors.textDark;
-    this.context.fillText('Game over!', x + 60, y + 27);
+    this.context.fillText(
+      'Game over!',
+      x + (this.sizeContainer - 260) / 2,
+      y + 27
+    );
 
     this.context.font = '30px sans-serif';
     this.context.fillStyle = this.colors.textDark;
-    this.context.fillText('Your score is', x + 100, y + 100);
+    this.context.fillText(
+      'Your score is',
+      x + (this.sizeContainer - 180) / 2,
+      y + 100
+    );
 
     this.context.font = '70px sans-serif';
     this.context.fillStyle = this.colors.textDark;
     const text = this.context.measureText(this.score);
     const textWidth = Math.round(text.width);
 
-    this.context.fillText(this.score, x + (400 - textWidth) / 2, y + 150);
+    this.context.fillText(
+      this.score,
+      x + (this.sizeContainer - textWidth) / 2,
+      y + 150
+    );
 
-    this.drawButton(x + 125, y + 300);
+    this.drawButton(x + (this.sizeContainer - 150) / 2, y + 300);
   }
 
   drawInfo() {
+    const sizeInfoWidth = 225;
+    const xInfo = this.xStart + (this.sizeContainer - sizeInfoWidth) / 2;
+
     this.context.beginPath();
     this.context.fillStyle = 'rgba(0,0,0,0.05)';
-    this.context.roundRect(600, 100, 300, 200);
+    this.context.roundRect(xInfo, 10, sizeInfoWidth, 130);
     this.context.fill();
 
-    this.drawScore(600, 100);
-    this.drawHighScore(700, 100);
-    this.drawButton(600, 170);
+    this.drawScore(xInfo, 10);
+    this.drawHighScore(xInfo + 180 / 2, 10);
+    this.drawButton(xInfo, 70);
 
     // this.drawGameOver(200, 100);
+  }
+
+  drawControlButton(direction) {
+    const width = 80;
+    let x = this.xStart + (this.sizeContainer - width) / 2;
+    let y = this.yStart;
+    let textBtn = '';
+
+    switch (direction) {
+      case 'up':
+        y = y + 450;
+        textBtn = '🡡';
+        break;
+      case 'down':
+        y = y + 550;
+        textBtn = '🡣';
+        break;
+      case 'left':
+        x = x - 100;
+        y = y + 500;
+        textBtn = '🡠';
+        break;
+      case 'right':
+        x = x + 100;
+        y = y + 500;
+        textBtn = '🡢';
+        break;
+      default:
+        break;
+    }
+
+    this.context.beginPath();
+    this.context.fillStyle = this.colors.background;
+    this.context.roundRect(x, y, width, 50, [8]);
+    this.context.fill();
+
+    this.context.font = '38px sans-serif';
+    this.context.fillStyle = this.colors.textLight;
+    this.context.fillText(textBtn, x + 23, y + 10);
+
+    // register click
+    this.registeredClick['btnControl' + direction] = {
+      positionX: x,
+      positionY: y,
+      width: width,
+      height: 50,
+      callback: () => {
+        this.handlingAndCalculatingItemValue(direction);
+      },
+    };
+  }
+
+  drawControl() {
+    this.drawControlButton('up');
+    this.drawControlButton('down');
+    this.drawControlButton('left');
+    this.drawControlButton('right');
   }
 
   drawGame() {
     this.clearDraw();
     this.context.beginPath();
     this.context.fillStyle = this.colors.background;
-    this.context.roundRect(100, 100, this.sizeContainer, this.sizeContainer, [
-      10,
-    ]);
+    this.context.roundRect(
+      this.xStart,
+      this.yStart,
+      this.sizeContainer,
+      this.sizeContainer,
+      [10]
+    );
     this.context.fill();
 
     for (let i = 0; i < this.arr2D.length; i++) {
       for (let k = 0; k < this.arr2D[i].length; k++) {
         const { x, y, value } = this.arr2D[i][k];
-        this.renderItem(x, y, value);
+        this.drawItem(x, y, value);
       }
     }
 
     this.getAllItemHaveZeroValue();
 
     this.drawInfo();
+    this.drawControl();
 
-    if (this.gameOver) this.drawGameOver(200, 100);
+    if (this.gameOver) {
+      this.drawGameOver(this.xStart, 100);
+    }
 
     // https://stackoverflow.com/questions/49197700/es6-class-this-in-callback-of-requestanimationframe
     // requestAnimationFrame(this.drawGame.bind(this));
@@ -297,11 +422,12 @@ class Game {
     this.getAllItemHaveZeroValue();
     this.randomNewItem(); //max =8;
 
-    game.drawGame();
-    game.handleKeyDown();
+    this.drawGame();
+    this.handleKeyDown();
+    this.handleTouch();
   }
 
-  HandlingAndCalculatingItemValue(key) {
+  handlingAndCalculatingItemValue(key) {
     // convert arr row to col
     const converArrRowToCol = [];
     for (let h = 0; h < this.sizeGame; h++) {
@@ -373,7 +499,9 @@ class Game {
       }
     }
 
-    // console.log(converArrRowToCol);
+    this.getAllItemHaveZeroValue();
+    this.randomNewItem();
+    requestAnimationFrame(this.drawGame.bind(this));
   }
 
   logTableArr2D() {
@@ -398,26 +526,58 @@ class Game {
     window.addEventListener('keydown', (e) => {
       switch (e.key) {
         case 'ArrowUp':
-          this.HandlingAndCalculatingItemValue('up'); // up = true
+          this.handlingAndCalculatingItemValue('up');
           break;
         case 'ArrowRight':
-          this.HandlingAndCalculatingItemValue('right'); // right = true
+          this.handlingAndCalculatingItemValue('right');
           break;
         case 'ArrowLeft':
-          this.HandlingAndCalculatingItemValue('left'); // right = false
+          this.handlingAndCalculatingItemValue('left');
           break;
         case 'ArrowDown':
-          this.HandlingAndCalculatingItemValue('down'); // up = true
+          this.handlingAndCalculatingItemValue('down');
           break;
         default:
           break;
       }
-      this.getAllItemHaveZeroValue();
-      this.randomNewItem();
-      // this.drawGame();
+    });
+  }
 
-      // this.logTableArr2D();
-      requestAnimationFrame(this.drawGame.bind(this));
+  handleTouch() {
+    let x = 0;
+    let y = 0;
+
+    window.addEventListener('touchstart', (e) => {
+      const { clientX, clientY } = e.targetTouches[0];
+      x = clientX;
+      y = clientY;
+    });
+
+    window.addEventListener('touchend', (e) => {
+      const { clientX, clientY } = e.changedTouches[0];
+
+      const x1 = clientX - x;
+      const y1 = clientY - y;
+
+      if (y1 <= 50 && x1 <= 50) return;
+
+      if (Math.abs(x1) > Math.abs(y1)) {
+        if (x1 > 0) {
+          // console.log('right');
+          this.handlingAndCalculatingItemValue('right');
+        } else {
+          // console.log('left');
+          this.handlingAndCalculatingItemValue('left');
+        }
+      } else {
+        if (y1 > 0) {
+          // console.log('up');
+          this.handlingAndCalculatingItemValue('up');
+        } else {
+          // console.log('down');
+          this.handlingAndCalculatingItemValue('down');
+        }
+      }
     });
   }
 
@@ -448,5 +608,5 @@ class Game {
 const game = new Game(ctx, 4, innerWidth, innerHeight);
 
 game.startGame();
-// game.testMode();
+game.testMode();
 game.addClick(canvas);
